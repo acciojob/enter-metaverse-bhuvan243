f61e772207064db52808fb919af207d523bad1af
Bhuvaneshwaran <125134187+bhuvan243@users.noreply.github.com>
2023-09-21 13:43:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const paraText = document.getElementById("status");
+const enterButton = document.getElementById("enterBtn");
+
+enterButton.addEventListener("click", ()=> {
+	let h1 = document.createElement("h1");
+	h1.innerText = "Entered Metaverse";
+	document.body.insertBefore(h1, paraText);
+	paraText.remove();
+}	
+);
